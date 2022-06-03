e8ef4536ef6149ab4c4e93e65d9350ff821a3a03
Thor-ubuntu <Thor@ubuntu>
2022-06-03 12:37:36 +0200

    added code

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name="Edn Configs",
+    version="0.0.1",
+    description="Edn enhanced config parser",
+    long_description="",
+    author="Massimiliano Falzari",
+    author_email="m.falzari@student.rug.nl",
+    url="https://github.com/vimmoos/edn_conf",
+    download_url="https://github.com/vimmoos/edn_conf",
+    license="LGPLv3",
+    install_requires=["edn_format"],
+    packages=find_packages(),
+)
