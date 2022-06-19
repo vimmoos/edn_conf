8e454d5630edfd7257e0c005591e058bd61354ac
Thor-ubuntu <Thor@ubuntu>
2022-06-19 11:05:15 +0200

    adding relative and abs paths

--- edn_conf/core.py
+++ edn_conf/core.py
@@ -10,13 +10,22 @@ def load(
     filename: str,
     basefolder: str,
     tags_filename: str = f"{os.path.dirname(os.path.abspath(__file__))}/tags",
+    relative: bool = False,
+    extension: bool = False,
 ):
     """Load an edn file with the reader macro defined in tags.edn."""
     with open(f"{tags_filename}.edn", "r") as f:
         tags = e.clojure_eval(edn.loads("".join(f.readlines())))
         for name, f in tags.items():
             edn.add_tag(name, f)
-    with open(f"./{basefolder}/{filename}.edn", "r") as f:
+    extension = ".edn" if not extension else ""
+    path = (
+        f"./{basefolder}/{filename}{extension}"
+        if relative
+        else f"{basefolder}/{filename}{extension}"
+    )
+
+    with open(path, "r") as f:
         return edn.loads("".join(f.readlines()))
 
 
